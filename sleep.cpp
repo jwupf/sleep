4a55b033b387df34d019af8ec24c6c978d8f6418
Jörg Wunderlich-Pfeiffer <jwupf@gmx.net>
2022-07-04 20:35:38 +0200

    Initial sleep exe

---
+++ sleep.cpp
@@ -0,0 +1,43 @@
+﻿// sleep.cpp : Defines the entry point for the application.
+//
+
+#include "sleep.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string help = "This program only supports one parameter, the time to sleep in seconds as an `unsigned long long`!";
+	if (argc != 2) {
+		cout << "This program only takes one argument!" << endl;
+		cout << help << endl;
+		return 1;
+	}
+
+	try {
+		size_t parsedChars = 0;
+		unsigned long long sleep_seconds = stoull(argv[1], &parsedChars);
+		if (strlen(argv[1]) != parsedChars) {
+			cout << "Not a propper unsigned long long as time in seconds to wait!" << endl;
+			cout << help << endl;
+			return 1;
+		}
+		cout << "Sleeping for: " << sleep_seconds << " seconds!" << endl;
+		// sleep here ?
+		typedef std::chrono::duration<int> seconds_type;
+		seconds_type sleep_duration(sleep_seconds);
+		this_thread::sleep_for(sleep_duration);
+	}
+	catch (invalid_argument ia_err) {
+		cout << "The given argument is not in a valid form!" << endl;
+		cout << help << endl;
+		return 1;
+	}
+	catch (out_of_range oor_err) {
+		cout << "The given number is to big!" << endl;
+		cout << help << endl;
+		return 1;
+	}
+
+	return 0;
+}
---
+++ sleep.h
@@ -0,0 +1,10 @@
+﻿// sleep.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// TODO: Reference additional headers your program requires here.
